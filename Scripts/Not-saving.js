ba8a67350f75d8363c4dcb537c85857f79ff6c39
GameBoo-x <abasfakre81@gmail.com>
2024-11-21 13:01:48 +0200

    Update Not-saving.js

--- Scripts/Not-saving.js
+++ Scripts/Not-saving.js
@@ -8,3 +8,27 @@
             element.addEventListener("mousedown", e => e.preventDefault());
             element.addEventListener("touchstart", e => e.preventDefault());
         });
+
+
+
+// إظهار الشاشة السوداء عند أخذ اسكرين شوت
+function showBlackScreenOverlay() {
+  const overlay = document.createElement('div');
+  overlay.classList.add('screenshot-overlay');
+  document.body.appendChild(overlay);
+  overlay.style.display = 'block';
+
+  // إخفاء الطبقة بعد فترة قصيرة
+  setTimeout(() => {
+    overlay.style.display = 'none';
+    document.body.removeChild(overlay);
+  }, 1000); // مدة التغطية: ثانية واحدة
+}
+
+// محاكاة اكتشاف اخذ الاسكرين شوت
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'PrintScreen') { // تحقق إذا تم الضغط على مفتاح PrintScreen
+    showBlackScreenOverlay();
+  }
+});
+
